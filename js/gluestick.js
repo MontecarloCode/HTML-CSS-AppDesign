4de27ba1d9cb943f8d382586d83a0c2570b6ed5e
montecarlocode <monte00829@gmail.com>
2022-07-17 13:02:51 +0800

    send prototype

--- js/gluestick.js
+++ js/gluestick.js
@@ -46,11 +46,6 @@ function toogle(id) {
     if (show_sign == true) openSign();
     else closeDropLeft(id);
   }
-  if (id == "chat-styles") {
-    show_chatStyles = !show_chatStyles;
-    if (show_chatStyles == true) openChatStyles();
-    else closeDropDown(id);
-  }
 }
 
 function openActions() {
@@ -92,4 +87,30 @@ function openSign() {
 function closeDropLeft(id) {
   if (id == "sign") show_sign = false;
   document.getElementById(id).style.display = "none";
+}
+
+function toogleChatStyles(element) {
+  let id = element.dataset.slave;
+  if (id == "chat-styles") {
+    show_chatStyles = !show_chatStyles;
+    if (show_chatStyles == true) openChatStyles(element);
+    else closeDropDown(id);
+  }
+}
+
+function openChatStyles(element) {
+  let prop = "bottom";
+  let h = "33vh";
+  let sh = screen.height;
+  let start_top = element.offsetTop;
+  let start_left = element.offsetLeft;
+  start_point_y = start_top + element.offsetHeight / 2;
+  let slave = element.dataset.slave;
+  let ele = document.getElementById(slave);
+  ele.style.width = 0;
+  ele.style[prop] = sh - start_point_y + "px";
+  ele.style.left = start_left + "px";
+  ele.style.height = h;
+  console.log(element.offsetWidth);
+  ele.style.width = element.offsetWidth + "px";
 }
